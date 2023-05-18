13d82ce665cf5d9c657ac9a5d9fda076572929ea
Yash Jain <yashphulfagar@gmail.com>
2023-05-18 17:40:50 +0530

    courses page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './page/homepage.page'
 import CoursesPage from './page/courses.page'
+import SearchforCourses from './page/searchCourses.page'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="courses/" element={<SearchforCourses />}></Route>
       <Route path="courses/BMAT205L" element={<CoursesPage />}></Route>
     </Routes>
   )
