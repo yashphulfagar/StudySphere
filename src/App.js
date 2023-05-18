5b6416a5ee9782fc33fd4238ffb41252680a46b1
Yash Jain <yashphulfagar@gmail.com>
2023-05-18 15:52:24 +0530

    content demo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="courses" element={<CoursesPage />}></Route>
+      <Route path="courses/BMAT205L" element={<CoursesPage />}></Route>
     </Routes>
   )
 }
--- src/page/courses.page.jsx
+++ src/page/courses.page.jsx
@@ -1,14 +1,14 @@
 import NavBar from "../components/NavBar.component";
 import ModuleBar from "../components/sidebar.component";
-
+import { Routes, Route } from "react-router-dom";
 
 const CoursesPage = () => {
-    return(
-        <>
-        <NavBar />
-        <ModuleBar />
-        </>
-    )
-}
+  return (
+    <>
+      <NavBar />
+      <ModuleBar />
+    </>
+  );
+};
 
 export default CoursesPage;
