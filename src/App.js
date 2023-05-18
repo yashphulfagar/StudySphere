eb0aba93a2b486b8ccbcc793cc6a4a0d68bea0b0
Yash Jain <yashphulfagar@gmail.com>
2023-05-18 11:28:15 +0530

    ---

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/homepage.pages'
+import HomePage from './page/homepage.page'
+import CoursesPage from './page/courses.page'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="courses" element={<CoursesPage />}></Route>
     </Routes>
   )
 }
