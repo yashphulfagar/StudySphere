ec698738a7c3d7a1f9ee902e1b39f2735da699f9
Yash Jain <yashphulfagar@gmail.com>
2023-05-20 10:37:22 +0530

    modulebar automated

--- src/components/sidebar.component.jsx
+++ src/components/sidebar.component.jsx
@@ -130,22 +130,24 @@ const ModuleBar = () => {
               modulesData={modulesData}
             />
 
-            <div class="inline-flex rounded-md shadow-sm" role="group">
-              <button
-                onClick={() => handleModuleClickPrev(selectedModuleId)}
-                type="button"
-                class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-              >
-                Previous
-              </button>
+            <div class="flex justify-center">
+              <div class="inline-flex rounded-md shadow-sm" role="group">
+                <button
+                  onClick={() => handleModuleClickPrev(selectedModuleId)}
+                  type="button"
+                  className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+                >
+                  Previous
+                </button>
 
-              <button
-                onClick={() => handleModuleClickNext(selectedModuleId)}
-                type="button"
-                class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-              >
-                Next
-              </button>
+                <button
+                  onClick={() => handleModuleClickNext(selectedModuleId)}
+                  type="button"
+                  className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+                >
+                  Next
+                </button>
+              </div>
             </div>
           </div>
         </div>
