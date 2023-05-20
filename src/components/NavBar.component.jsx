3b0003ed711dfa8092d54dd394b0a5820505e24e
Yash Jain <yashphulfagar@gmail.com>
2023-05-20 10:39:59 +0530

    next and prev buttons

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -4,15 +4,15 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
 const navigation = [
-  { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
-  { name: "Courses", href: "/courses" },
+  { name: "Home", href: "#" },
+  { name: "About", href: "#" },
+  { name: "Courses", href: "#" },
 ];
 
 export default function NavBar({ courses }) {
-  let barType;
+  let barType, barColor;
   if (courses === "1") {
-    barType = `fixed inset-x-0 top-0 z-30 bg-gray-50 dark:bg-gray-900`;
+    barType = `fixed inset-x-0 top-0 z-50 bg-gray-50 dark:bg-gray-900`;
   } else {
     barType = `absolute inset-x-0 top-0 z-50`;
   }
@@ -28,26 +28,35 @@ export default function NavBar({ courses }) {
             className="flex items-center justify-between p-6 lg:px-8"
             aria-label="Global"
           >
-            <div className="flex lg:flex-1"></div>
+            <div className="flex lg:flex-1">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+            </div>
             <div className="flex lg:hidden">
-              <Link
+              <button
                 type="button"
                 className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                 onClick={() => setMobileMenuOpen(true)}
               >
                 <span className="sr-only">Open main menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-              </Link>
+              </button>
             </div>
             <div className="hidden lg:flex lg:gap-x-12">
               {navigation.map((item) => (
-                <Link
+                <a
                   key={item.name}
                   href={item.href}
                   className="text-sm font-semibold leading-6 text-gray-900"
                 >
                   {item.name}
-                </Link>
+                </a>
               ))}
             </div>
           </nav>
@@ -89,6 +98,14 @@ export default function NavBar({ courses }) {
                         {item.name}
                       </a>
                     ))}
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Log in
+                    </a>
                   </div>
                 </div>
               </div>
